9361cf5a7e323e7fe3717a65e94290c37a9deeb5
Jorge Fuentes <133913508+fuentesjs@users.noreply.github.com>
2024-04-07 02:39:17 -0400

    Update team3vhp.py

--- team3vhp.py
+++ team3vhp.py
@@ -1,1 +1,11 @@
+from ast import And
 import streamlit as st
+
+st.title("Virtual Human Advisor")
+
+prompt = st.chat_input("Enter your query")
+
+response = 'The requirements for query vary.  Please consult with your advisor'
+
+if prompt and response:
+    st.write(response)
