a253e50fb18ccb2d148a4b5d69ab2e27a9348d81
Jorge Salomon Fuentes <fuentesjs@yahoo.com>
2024-05-06 02:36:09 -0400

    Update st_vhp5_2.py
    Update name of package for llama_index.vector_stores.qdrant

--- st_vhp5_2.py
+++ st_vhp5_2.py
@@ -2,7 +2,7 @@ from llama_index.core import StorageContext, load_index_from_storage
 from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.core import Settings
 from llama_index.core import VectorStoreIndex
-from llama_index_vector_stores_qdrant import QdrantVectorStore
+from llama_index.vector_stores.qdrant import QdrantVectorStore
 import qdrant_client
 from qdrant_client import QdrantClient
 import streamlit as st
